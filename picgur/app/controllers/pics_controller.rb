c1cc7f2986d97d169560ae7fb75d387a10736631
Your Name <sylviapereira@gmail.com>
2016-01-26 18:43:48 -0500

    Making pictures show at /pictures

--- picgur/app/controllers/pics_controller.rb
+++ picgur/app/controllers/pics_controller.rb
@@ -1,4 +1,5 @@
 class PicsController < ApplicationController
   def index
+    @pic = Pic.all
   end
 end
--- picgur/db/seeds.rb
+++ picgur/db/seeds.rb
@@ -1,3 +1,21 @@
+Pic.create!(
+  :art_maker => "Bob Ross",
+  :art_name => "Secret Bush",
+  :url => "http://www.bobrossfacts.com/images/randomPaintings/1.jpg"
+)
+
+Pic.create!(
+  :art_maker => "Boeb Roess",
+  :art_name => "Some Mountains",
+  :url => "https://usatunofficial.files.wordpress.com/2012/07/bob-ross.jpg"
+)
+
+Pic.create!(
+  :art_maker => "Boboo Rooss",
+  :art_name => "Range",
+  :url => "http://images.summitpost.org/original/188791.jpg"
+)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
